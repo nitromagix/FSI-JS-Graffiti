81bc66e048eb128bbd499d1a09acbffcea5a8e28
Martin <nitromagix@gmail.com>
2022-02-02 19:00:42 -0800

    playing with querySelector

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Select some elements...
+
+let header = document.querySelector('#page-header')
+header.style.textAlign = 'left'
+header.style.color = 'grey'
+header.style.borderTop = '3px double black'
+
+let dogImages = document.querySelectorAll('.dog-image')
+for (let i = 0; i < dogImages.length; i++)
+{
+    dogImages[i].style.borderRadius = '50px'
+}
+
+let paragraphs = document.querySelectorAll('.dog-description')
+for (let i = 0; i < paragraphs.length; i++)
+{
+    paragraphs[i].style.textAlign = 'justify' 
+    paragraphs[i].style.textIndent = '10px' 
+}
+
+let footer = document.querySelector('.footer')
+footer.style.border = "1px solid black"
+
